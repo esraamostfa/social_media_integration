d4d6c96f2b60dfd0a42b93c9701b0d1fb62379bb
esraamostfa <esraamostfa13@gmail.com>
2022-02-12 23:05:18 +0200

    login screen design

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -6,8 +9,104 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           Padding(
+            padding: const EdgeInsetsDirectional.only(start: 19.0, top: 77),
+            child: Text(
+                'Welcome...',
+              style: GoogleFonts.lato(textStyle:  TextStyle(
+                  color: Colors.blue[300],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 33,
+                  fontStyle: FontStyle.italic,
+              )),
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                MaterialButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(MdiIcons.google, color: Colors.white,),
+                      //SizedBox(width: 25,),
+                      Padding(
+                        padding: EdgeInsetsDirectional.only(start: 19.0, end: 15),
+                        child: Text(
+                            'Continue with Google',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 21
+                        ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  color: Colors.amberAccent,
+                  minWidth: 291,
+                  height: 51,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                const SizedBox(height: 15,),
+                MaterialButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(MdiIcons.facebook, color: Colors.white,),
+                        SizedBox(width: 11,),
+                        Text(
+                            'Continue with Facebook',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 21
+                            )),
+                      ],
+                    ),
+                    color: Colors.blueAccent,
+                    minWidth: 291,
+                    height: 51,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))
+                ),
+                const SizedBox(height: 15,),
+                MaterialButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(
+                            MdiIcons.github,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 11,),
+                        Text(
+                            'Continue with GITHUB',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 21
+                            )
+                        ),
+                      ],
+                    ),
+                    color: Colors.blueGrey,
+                    minWidth: 291,
+                    height: 51,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))
+                ),
+              ],
+            ),
+          ),
+          Image(image: AssetImage('assets/images/dash.png'),)
+        ],
+      ),
     );
   }
 }
