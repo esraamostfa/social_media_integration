aedeac4c934e758078193c881d7506cc2eee12ce
esraamostfa <esraamostfa13@gmail.com>
2022-02-12 20:00:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# social_media_integration
+
+An app that allow user to login usion his sochial media accounts and display his profile data.
---
+++ android/app/src/main/kotlin/com/esraa13/grip/tsf/social_media_integration/social_media_integration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.esraa13.grip.tsf.social_media_integration.social_media_integration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
